92ee5c44e9f3bb561c84f8c90abbf35e1f22f605
fxyoge <peanut@fxyoge.com>
2024-08-31 18:12:18 -0500

    remove some comments - this was just me fighting with the computer

--- modules/system/flatpak/default.nix
+++ modules/system/flatpak/default.nix
@@ -9,18 +9,6 @@
       onCalendar = "weekly";
     };
   };
-
-  # imports = [
-  #   inputs.nix-flatpak.nixosModules.nix-flatpak
-  # ];
-
-           # gmodena/nix-flatpak #25
-
-
-  # # System flatpaks are managed by nix. User flatpaks may be managed by
-  # # the user, independent from nix, allowing for local builds and other
-  # # experimentation/shenanigans.
-  # services.flatpak.uninstallUnmanaged = true;
   
   services.flatpak.enable = true;
   xdg.portal.enable = true;
