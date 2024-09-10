2114b92b039b7f4e9f153b6bcade92719d753845
fxyoge <peanut@fxyoge.com>
2024-09-09 19:28:26 -0500

    refactors that i should have made a long time ago

--- modules/finance/finreport/home/default.nix
+++ modules/finance/finreport/home/default.nix
@@ -1,36 +1,14 @@
 { pkgs, ... }: let
-  # TODO: This script is remarkably inefficient and the journal should be restructured
-  #       to take advantage of `include`s.
-  hledger-wrapper = (pkgs.writeShellScriptBin "hledger-wrapper" ''
-    set -e
-
-    journal_path="$HOME/Repos/fxyoge/finance"
-
-    if [ ! -d "$journal_path" ]; then
-      echo "journal does not exist"
-      exit 1
-    fi
-
-    agg_journal="/tmp/$(uuidgen)"
-    touch "$agg_journal"
-    trap "rm '$agg_journal'" EXIT
-
-    find "$journal_path" -type f -name "*.journal" | sort | while read -r file; do
-      cat "$file" >> "$agg_journal"
-      echo >> "$agg_journal"
-    done
-
-    LEDGER_FILE="$agg_journal" ${pkgs.hledger}/bin/hledger "$@"
-  '');
-  hledger-wrapper-bin = "${hledger-wrapper}/bin/hledger-wrapper";
   finreport-accounts = (pkgs.writeShellScriptBin "finreport-accounts" ''
-    ${hledger-wrapper-bin} -I accounts --types
+    journal_path="$HOME/Repos/fxyoge/finance/0_all"
+    ${pkgs.hledger}/bin/hledger -f "$journal_path" -I accounts --types
   '');
   finreport-bs = (pkgs.writeShellScriptBin "finreport-bs" ''
     set -e
     options=$(getopt -o p: -l period: -n "$0" -- "$@") || exit
     eval set -- "$options"
     arg_period="quarterly from 12 quarters ago"
+    journal_path="$HOME/Repos/fxyoge/finance/0_all"
     while [[ $1 != -- ]]; do
       case $1 in
         -p|--period) arg_period="$2"; shift 2;;
@@ -38,13 +16,14 @@
       esac
     done
     shift
-    ${hledger-wrapper-bin} balancesheet -V --infer-value --period "$arg_period"
+    ${pkgs.hledger}/bin/hledger balancesheet -f "$journal_path" -V --infer-value --period "$arg_period"
   '');
   finreport-is = (pkgs.writeShellScriptBin "finreport-is" ''
     set -e
     options=$(getopt -o p: -l period: -n "$0" -- "$@") || exit
     eval set -- "$options"
     arg_period="quarterly from 12 quarters ago"
+    journal_path="$HOME/Repos/fxyoge/finance/0_all"
     while [[ $1 != -- ]]; do
       case $1 in
         -p|--period) arg_period="$2"; shift 2;;
@@ -52,7 +31,7 @@
       esac
     done
     shift
-    ${hledger-wrapper-bin} incomestatement -V --infer-value --period "$arg_period"
+    ${pkgs.hledger}/bin/hledger incomestatement -f "$journal_path" -V --infer-value --period "$arg_period"
   '');
 in {
   home.packages = [
