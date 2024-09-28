67557ca9bc899f74818e652e9d994bdbbf63d1a3
fxyoge <peanut@fxyoge.com>
2024-09-28 17:57:03 -0500

    it'll work i'm sure

--- packages/setup-nixos/default.nix
+++ packages/setup-nixos/default.nix
@@ -1,1 +1,11 @@
-{ pkgs, ... }: pkgs.writeShellScriptBin "setup-nixos" ./setup-nixos.sh
+{ pkgs, ... }: let
+    pname = "setup-nixos";
+in pkgs.symlinkJoin {
+    name = pname;
+    paths = with pkgs; [
+        (writeShellScriptBin pname ./setup-nixos.sh)
+        git
+    ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = "wrapProgram $out/bin/${pname} --prefix PATH : $out/bin";
+}
